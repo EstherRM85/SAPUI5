010beda23374a3193a51f0091a735768a6306ff1
EstherRM85 <estherruizmartinez1985@gmail.com>
2020-11-07 11:46:35 +0000

    segundo commit

---
+++ MyProyecto/webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("Logali.MyProyecto.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
